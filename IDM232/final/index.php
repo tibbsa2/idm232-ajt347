0b9582e9dd1a622bda00f4d38210873cc3fcec00
Alexis T <alexistibbs01@gmail.com>
2022-11-17 04:49:22 -0500

    W9

--- IDM232/final/index.php
+++ IDM232/final/index.php
@@ -36,3 +36,26 @@ include_once __DIR__ . '/_components/header.php';
 </main>
 <?php include_once __DIR__ . '/_components/footer.php';
 ?>
+
+<!--
+    My section for notes
+    Search for exact text. 
+    search box, form tag, name and id 
+
+    how to do search box by entering instead of clicking a button? 
+
+    this is an admin side example. 
+    how to make the search bar work for one area, so when you enter it will redirect to one page and not 
+
+    15:12 zoom screen screening: sending to a file 
+    Make the method a GET method instead of POST for search.. 
+
+    in adming there's a search folder with index.php
+
+    Up till 22:00 added the value we search against when form is submitted (var is called $search
+    22:00 < syntax weird % syntax & LIKE Key word 
+
+    How to get multiple images to pop up instead of just one image in the directory like sort through a list
+    
+
+ -->
